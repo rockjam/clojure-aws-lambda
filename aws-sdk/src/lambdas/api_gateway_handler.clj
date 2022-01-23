8077fae453bbb9b971c8ffab86ffddf2578ddbe4
Nikolai Tatarinov <5min4eq.unity@gmail.com>
2022-01-23 14:08:12 +0100

    add api-gateway-handler Lambda; move aot compilation to a separate clojure script

---
+++ aws-sdk/src/lambdas/api_gateway_handler.clj
@@ -0,0 +1,28 @@
+(ns lambdas.api-gateway-handler
+  (:gen-class
+   :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+  (:require [clojure.data.json :as json]
+            [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn handle-event [event]
+  (do
+    (println "event:" event)
+    {:statusCode 200
+     :headers    {"content-type" "text/plain; charset=utf-8"}
+     :body       "Hello World!"}))
+
+(defn key->keyword [key-string]
+  (-> key-string
+      (str/replace #"([a-z])([A-Z])" "$1-$2")
+      (str/replace #"([A-Z]+)([A-Z])" "$1-$2")
+      (str/lower-case)
+      (keyword)))
+
+(defn -handleRequest [this is os context]
+  (let [w (io/writer os)]
+    (-> (json/read (io/reader is) :key-fn key->keyword)
+        (handle-event)
+        (json/write w))
+    (.flush w)))
+
